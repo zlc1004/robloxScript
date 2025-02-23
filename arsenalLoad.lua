162a279a4eff746d99ea43bb30b79710e212688e
lucasz228 <91501777+zlc1004@users.noreply.github.com>
2025-02-22 16:45:15 -0800

    Update arsenalLoad.lua

--- arsenalLoad.lua
+++ arsenalLoad.lua
@@ -1,24 +1,37 @@
 local function send(text)
-	local StarterGui = game:GetService("StarterGui")
-	StarterGui:SetCore("SendNotification",{
-		Title = "Loading",
-		Text = text,
-		Duration = 5
-	})
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification",
+                       {Title = "Loading", Text = text, Duration = 5})
 end
 
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/AnnaRoblox/AnnaBypasser/refs/heads/main/AnnaBypasser.lua",true))()
+
 send("aimbot")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/aimbot.lua"))()
+loadstring(game:HttpGet(
+               "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/aimbot.lua",
+               true))()
 task.wait(1)
 send("Vapav2-Arsenal")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/Vapav2-Arsenal.lua"))()
+loadstring(game:HttpGet(
+               "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/Vapav2-Arsenal.lua",
+               true))()
 task.wait(1)
 send("esp")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/esp.lua"))()
+task.spawn(function()
+
+    loadstring(game:HttpGet(
+                   "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/esp.lua",
+                   true))()
+end)
+
 task.wait(1)
 send("loading fling in 5 seconds")
 
 task.wait(5)
 
 send("fling")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/fling.lua"))()
+task.spawn(function()
+    loadstring(game:HttpGet(
+                   "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/fling.lua",
+                   true))()
+end)
