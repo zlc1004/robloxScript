e013537e55db456aec820f7dca37f18381a291e5
lucasz228 <91501777+zlc1004@users.noreply.github.com>
2025-02-22 16:27:52 -0800

    Update arsenalLoad.lua

--- arsenalLoad.lua
+++ arsenalLoad.lua
@@ -1,5 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/fling.lua"))()
+function sleep(n)
+  local t = os.clock()
+  while os.clock() - t <= n do
+    -- nothing
+  end
+end
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/fling.lua"))()
+sleep(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/aimbot.lua"))()
+sleep(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/Vapav2-Arsenal.lua"))()
+sleep(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/esp.lua"))()
