6f9bfd516fa3466dac6db568a2193eb76114c356
lucasz228 <91501777+zlc1004@users.noreply.github.com>
2025-02-23 20:26:19 -0800

    Update arsenalLoad.lua

--- arsenalLoad.lua
+++ arsenalLoad.lua
@@ -1,42 +1,38 @@
 local function send(text)
     local StarterGui = game:GetService("StarterGui")
     StarterGui:SetCore("SendNotification",
-                       {Title = "Loading", Text = text, Duration = 5})
+                       {Title = "Message", Text = text, Duration = 5})
 end
 
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/arsenalLoad.lua",true))()
 
-send("aimbot")
-loadstring(game:HttpGet(
-               "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/aimbot.lua",
-               true))()
-task.wait(1)
-send("Vapav2-Arsenal")
-loadstring(game:HttpGet(
-               "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/Vapav2-Arsenal.lua",
-               true))()
-task.wait(1)
-send("esp")
+send("W hacks by @lzz228")
+
+send("Loading aimbot")
+task.spawn(function()
+
+    loadstring(game:HttpGet(
+                   "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/aimbot.lua",
+                   true))()
+end)
+-- task.wait(1)
+send("Loading Vapav2-Arsenal")
+task.spawn(function()
+
+    loadstring(game:HttpGet(
+                   "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/Vapav2-Arsenal.lua",
+                   true))()
+end)
+-- task.wait(1)
+send("Loading ESP")
 task.spawn(function()
 
     loadstring(game:HttpGet(
                    "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/esp.lua",
                    true))()
 end)
-
-task.wait(1)
-send("loading fling in 5 seconds")
-task.wait(1)
-send("loading fling in 4 seconds")
-task.wait(1)
-send("loading fling in 3 seconds")
-task.wait(1)
-send("loading fling in 2 seconds")
-task.wait(1)
-send("loading fling in 1 seconds")
-task.wait(1)
-
-send("fling")
+-- task.wait(1)
+send("Loading fling")
 task.spawn(function()
     loadstring(game:HttpGet(
                    "https://raw.githubusercontent.com/zlc1004/robloxScript/refs/heads/main/fling.lua",
